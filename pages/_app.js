79af1307fd6ebaa48f63cd1c6f9ba50d6d24e6f5
danielhdev <d.huebschmann@tutanota.com>
2023-03-22 14:33:27 +0100

    updated meta data

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Contacticons from '@components/Contacticons'
 import Navbar from '@components/Navbar'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 import Lenis from '@studio-freight/lenis'
 
 
@@ -25,6 +26,18 @@ function Application({ Component, pageProps }) {
   }
   return (
     <>
+      <Head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="digital design brand design web design brands branding" />
+        <meta name="description" content="Digitaldan Agency offer brand design and digital design, including websites, online adverstisement, design for events and presentations." />
+        <meta http-equiv="X-UA-Compatible" content="IE=7" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <title>Digitaldan Agency, digital design, brand design, creating digital values</title>
+      </Head>
       <Navbar />
       <Contacticons />
       <AnimatePresence mode='wait'
