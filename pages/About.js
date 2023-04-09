40d983a69114581a32191b49656bc8438559f85b
danielhdev <d.huebschmann@tutanota.com>
2023-04-09 19:06:22 +0200

    font fix

--- pages/About.js
+++ pages/About.js
@@ -15,7 +15,7 @@ const About = () => {
                 id="About"
                 className="relative h-auto py-[10vh]">
                 <motion.h1
-                    className="aboutheader ml-[5%]"
+                    className="paragraph font-bold ml-[5%]"
                     initial={{ opacity: 0 }}
                     whileInView={{ opacity: 1 }}
                     transition={{ delay: 0.5, duration: 0.5 }}
