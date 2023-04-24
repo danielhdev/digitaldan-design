31a84d7abf3784b99d17baa9d2df14132bbcde6c
danielhdev <d.huebschmann@tutanota.com>
2023-04-24 08:43:20 +0200

    new header

--- components/Hero.js
+++ components/Hero.js
@@ -26,19 +26,21 @@ const Hero = () => {
     return (
         <main className="h-screen flex flex-col 
         justify-center items-center">
-            <motion.section className="z-50 w-full px-[1rem] md:px-[2rem]">
+            <motion.section className="z-50 w-full md:px-[2rem]">
 
                 <section className="paragraph bg-orange-500 dark:bg-zinc-100">
 
-                    <div className="text-[#041320]">
-                        <motion.h1 animate={wedesignAnimate} className="heroheader font-bold">We Design</motion.h1>
-                        <motion.h1 animate={wedesignAnimate2} className="heroheader font-bold">Quality.</motion.h1>
+                    <div className="text-[#041320] flex flex-col md:flex-row">
+                        <div className="flex flex-col">
+                            <motion.h1 animate={wedesignAnimate} className="heroheader font-bold w-[20%]">We Design</motion.h1>
+                            <motion.h1 animate={wedesignAnimate2} className="heroheader font-bold">Quality.</motion.h1></div>
                         <br />
-                        <motion.h2 animate={servicesAnimate}>
+                        <motion.h2 animate={servicesAnimate} className="lg:w-[50%] header
+                        text-zinc-100 dark:text-zinc-900">
                             Digital Design & Development<br />
                             Brand & Graphic Design</motion.h2></div>
                 </section>   <motion.section animate={wedesignAnimate2}
-                    className="flex flex-row justify-between herolinks font-bold">
+                    className="flex flex-row justify-between herolinks font-bold px-[2rem]">
 
                     <Link href="/Work">
                         <h2 className=" text-amber-600 cursor-pointer"> work <BsArrowUpRight /></h2>
