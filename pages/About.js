f96327715e15dc21ab1d96099540c413833aadb6
danielhdev <d.huebschmann@tutanota.com>
2023-04-24 21:24:43 +0200

    service overhaul

--- pages/About.js
+++ pages/About.js
@@ -15,30 +15,27 @@ const About = () => {
                 className="h-auto pt-[2.5rem] flex flex-col justify-center">
                 <section className="paragraph flex justify-center items-center md:py-[2.5rem]">
                     <div className="aboutcontainer w-[95vw] lg:w-[70vw]">
-
                         <motion.div animate={paragraphAnimate}>
                             <section className="h-auto py-[10vh] lg:w-[65ch] text-[var(--stone) px-[4vw]">
                                 <h1 className="header">About</h1>
                                 <br />
                                 "...Every detail is important because the end result is the sum of all details..." "...Quality is there or is not there, and if it is not there we have lost our time..."
                                 <br /> <span className="italic">Massimo Vignelli, from The Vignelli Canon</span>
-                                <br/>
-                                <br/>
-                                Daniel Hubschmann (Germany), freelancer, senior multidisciplinary designer and web developer.
-                                Helping clients with my over two decades of experience.   In my remote-only agency I work with professionals from around the world.
                                 <br />
                                 <br />
-                                <span className="font-bold">I believe in timeless design over trends, using classic rules.
-                                    Your investment will pay off for many years thanks to great, timeless design.</span>
+                                Daniel Hubschmann (Germany), freelancer, senior digital designer and web developer.
+                                After over two decades in design I changed careers to fullstack web development in recent years.
                                 <br />
+                                <br />
+                                <h1 className="header">Agency for creative agencies</h1>
+                                <br />
+                                <div>Helping creative agencies around to world turning their ideas into well performing code.
+                                    In addition my agency offers digital design services to help you reduce your work load.</div>
                                 <br />
                                 <ul className="header list-disc">
                                     Services:
-                                    <li>Art direction</li>
-                                    <li>Brand design</li>
-                                    <li>Digital design <br /> (Websites, apps, ads, <br />presentations and events) </li>
                                     <li>Digital development <br />(JavaScript and other tools)</li>
-                                    <li>Graphic design</li>
+                                    <li>Digital design <br /> (Websites, apps, ads, <br />presentations and events) </li>
                                 </ul>
                                 <br />  <Link href="/Contact">
                                     <h2 className="header text-amber-600 cursor-pointer">contact <BsArrowUpRight /> </h2>
--- pages/Case2/DDbranding.js
+++ pages/Case2/DDbranding.js
@@ -13,10 +13,9 @@ const DDbranding = () => {
                         Digitaldan Agency/<br /> Personal Branding
                     </h1 >
                     <ul className="paragraph font-bold md:ml-[10vw]">
-                        <li>Art Direction</li>
-                        <li>Graphic Design</li>
-                        <li>Digital Design</li>
-                        <li>Digital Development</li>
+                   
+                        <li>Digital design with Figma</li>
+                        <li>Digital development with Next.js</li>
                     </ul>
                 </section>
 
@@ -26,93 +25,18 @@ const DDbranding = () => {
                             Design Thoughts
                         </h1>
                     </div>
-
                     <article className="md:ml-[10vw] md:w-[70ch] p-4">
                         <p className="whitespace-normal paragraph font-[Oxygen] pt-[5vh]">
-                            <span className="font-bold">A new brand design for my agency:<br /></span>
+                            <span className="font-bold">Digital design & development for my agency:<br /></span>
                             I used my personal style that can be best described as expressive minimalism, based on the teachings of the 20th century greats like Vignelli.
                             Also used AI for the first time: DALL-E provided some of the images.
                         </p>
-                        <br />
-                        <br />
-
-
-                    </article>
-                    <section className="h-auto w-[95vw]">
-                        <h1 className="font-[octarine] header my-[5vh] md:my-[15vh] p-4">
-                            Typography
-                        </h1>
-                        <div className="h-auto flex justify-center items-start flex-col ml-[10vw]">
-                            <h1 className="fonts font-[octarine] font-light my-[2.5vh]">
-                                Octarine
-                            </h1>
-                            <h1 className="fonts font-[Oxygen] font-light my-[2.5vh]">
-                                Oxygen
-                            </h1>
-                        </div>
-                    </section>
-
+                    </article>          
                     <section className="h-auto w-[95vw] my-[5vh] md:my-[15vh] p-4 mx-auto">
                         <h1 className="font-[octarine] header">
-                            Brand Colors
+                            Social Media Assets
                         </h1>
-                        <h2 className="paragraph py-[10vh] md:ml-[10vw]">Amber</h2>
-                        <div
-                            className="md:w-[55vw] h-auto flex flex-row
-          paragraph md:ml-[10vw]"
-                        >
-                            <div className="h-[30vh] flex-1 bg-[#ffbf00] text-stone-900">
-                                #ff8547
-                            </div>
-                            <div className="h-[30vh] flex-1 bg-[#ffd400] text-stone-900">
-                                #ff8547
-                            </div>
-                            <div className="h-[30vh] flex-1 bg-[#ffe900] text-stone-900">
-                                #ff8547
-                            </div>
-
-                        </div>
-                    </section>
-                    <section className="w-[95vw] h-auto mb-[10vh] mx-auto">
-                        <h2 className="paragraph py-[10vh] md:ml-[10vw]">
-                            Zinc
-                        </h2>
-                        <div
-                            className="md:w-[55vw] h-auto  flex flex-row justify-center 
-          paragraph md:ml-[10vw]"
-                        >
-                            <div className="h-[30vh] flex-auto bg-[#09090b] text-zinc-100 border-[1px] border-stone-100 border-solid">
-                                #09090b
-                            </div>
-                            <div className="h-[30vh] flex-auto bg-[#f3f5f6] text-stone-900 border-[1px] border-stone-900 border-solid">
-                                #f3f5f6
-                            </div>
-                        </div>
-                    </section>
-                    <section className="h-auto w-[95vw] my-[5vh] md:my-[15vh] p-4 mx-auto">
-                        <h1 className="font-[octarine] header">
-                            Logo
-                        </h1>
-                        <h2 className="paragraph py-[10vh] md:ml-[10vw]">The circles Octarine is made of will be part of public branding.</h2>
-                        <div
-                            className="md:w-[55vw] h-auto flex flex-col md:flex-row border-[1px] border-stone-900
-          workheader md:ml-[10vw]"
-                        >
-                            <div className="grid place-content-center p-8 md:p-2 h-[50vh] flex-1 bg-[#f3f5f6] text-stone-900">
-                                digital.dan
-                            </div>
-                            <div className="grid place-content-center p-8 md:p-2 h-[50vh] flex-1 bg-[#383630] text-zinc-100 tracking-widest">
-                                d.d
-                            </div>
-
-
-                        </div>
-                    </section>
-                    <section className="h-auto w-[95vw] my-[5vh] md:my-[15vh] p-4 mx-auto">
-                        <h1 className="font-[octarine] header">
-                            AI
-                        </h1>
-                        <h2 className="paragraph py-[10vh] md:ml-[10vw]">DALL-E images using Octarines circles and round elements <br />for social media branding.</h2>
+                        <h2 className="paragraph py-[10vh] md:ml-[10vw]">AI DALL-E images for social media branding.</h2>
                         <div className="md:w-[75%] flex justify-end my-[10vh] md:ml-[10vw]">
                             <img src='ai1.png'
                                 alt="Image of AI generated images using circles"
@@ -132,8 +56,7 @@ const DDbranding = () => {
                         <h1 className="font-[octarine] header">
                             Website
                         </h1>
-
-                        <div className="md:w-[45%] flex justify-end my-[15vh] md:ml-[15vw]">
+               <div className="md:w-[45%] flex justify-end my-[15vh] md:ml-[15vw]">
                             <img src='mobile1.png'
                                 alt="Image of a LinkedIn profile header"
                             /></div>
