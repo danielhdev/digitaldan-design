58c418af6da73bf5442fdc9f9c35b2dc9854e221
danielhdev <d.huebschmann@tutanota.com>
2023-04-27 11:12:17 +0200

    design overhaul

---
+++ components/Borders.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Borders = ({ children }) => {
+    return (
+        <div className="border-t-[2px] border-r-[2px] border-l-0 border-b-0 
+    border-stone-800/50 dark:border-zinc-50 
+    ">{children}</div>
+    )
+}
+
+export default Borders
