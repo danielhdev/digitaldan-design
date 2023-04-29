cc9719288eb6ee68828cee55aec3d7ad6473724b
danielhdev <d.huebschmann@tutanota.com>
2023-04-29 16:32:38 +0200

    layout fixes

--- components/ParallaxSection.js
+++ components/ParallaxSection.js
@@ -3,7 +3,7 @@ import { Parallax } from 'react-scroll-parallax'
 const ParallaxSection = ({ children }) => {
     return (
         <section
-            className="h-[25vh] bg-[#09090b] text-[#fdfdfd] overflow-hidden
+            className="h-[30vh] md:h-[50vh] bg-[#09090b] text-[#fdfdfd] overflow-hidden
         dark:bg-[#fdfdfd] dark:text-[#09090b] grid place-content-center"
         ><Parallax speed={-5} className="heroheader">
                 {children}
