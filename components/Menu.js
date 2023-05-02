a6b69785ace98f0b4cf5f49ee2b04eae4bd0cad9
danielhdev <d.huebschmann@tutanota.com>
2023-05-02 10:38:31 +0200

    ui updates

--- components/Menu.js
+++ components/Menu.js
@@ -61,7 +61,6 @@ const Menu = () => {
                                 )}
                             </ul>
                         </motion.div>)}
-
                 </AnimatePresence>
 
             </div>
