5f259ae3e3c1c2f8c83bc74333ff96d6572ee8a9
danielhdev <d.huebschmann@tutanota.com>
2023-05-02 09:02:58 +0200

    update design parallax sections

--- components/ParallaxSection.js
+++ components/ParallaxSection.js
@@ -3,11 +3,14 @@ import { Parallax } from 'react-scroll-parallax'
 const ParallaxSection = ({ children }) => {
     return (
         <section
-            className="h-[30vh] md:h-[50vh] bg-[#09090b] text-[#fdfdfd] overflow-hidden
-        dark:bg-[#fdfdfd] dark:text-[#09090b] grid place-content-center"
-        ><Parallax speed={-5} className="heroheader">
+            className="h-[20vh] md:h-[50vh] bg-[#09090b] overflow-hidden
+        dark:bg-[#fdfdfd] grid place-content-center relative"
+        ><Parallax speed={-5} className="heroheader z-20">
                 {children}
             </Parallax>
+            <Parallax speed={10} className="absolute h-[10vh] md:h-[25vh] inset-0 top-1/3 bg-[#fdfdfd] dark:bg-[#09090b] 
+                outline-4 outline-amber-500"></Parallax>
+
         </section>
     )
 }
