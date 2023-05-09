0802bb2ab0ba7ad1d19c74def0807bdaebe8dab3
danielhdev <d.huebschmann@tutanota.com>
2023-05-09 13:54:21 +0200

    sercies update

--- components/Services.js
+++ components/Services.js
@@ -2,26 +2,27 @@ const Services = () => {
 
     return (
         <>
-            <main id="About" className='h-auto py-[5rem]'>
-                <section className="flex justify-start px-4 mb-[2rem]">
+            <main id="About" className='h-auto grid place-content-center py-[5rem]'>
+                <section className="px-2 mb-[2rem]">
                     <ul className="herolinks 
                     list-disc p-[5vh] md:p-[10vh] landscape:w-[75vw]">
                         <li>Daniel Hubschmann (Germany)</li>
-                        <li>Web, graphic & digital designer for over 20 years</li>
-                        <li>Design: create cool stuff,
-                            get results and make money for all of us, no fluff, no phrases.</li>
+                        <li>Web, graphic & digital designer</li>
+                        <li>Over 20 years experience<br />
+                            getting results for clients</li>
                     </ul></section>
-                <section className="flex md:justify-end px-4">
+                <section className="flex md:justify-end px-2">
                     <ul className="herolinks list-disc p-[5vh] md:p-[10vh]
                       landscape:w-[75vw] mb-[4rem]">
 
                         Services
                         <li>Digital design and marketing</li>
-                        <li>Creating visual brands identities, <br />websites, social media ads </li>
+                        <li>Visual brands identities</li>
+                        <li>Apps, websites, landing pages</li>
+                        <li>Social media ads & content</li>
                         <li>Digital design for events,<br /> presentations, billboards</li>
                         <li>Web development</li>
                         <br />
-                        Your creative force for hire!<br />
                         Let&apos;s talk about your project:
 
 
