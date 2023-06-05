68af7811341c3b42cdad5a671491a08b64d20a4d
danielhdev <d.huebschmann@tutanota.com>
2023-06-05 16:18:23 +0200

    imges update 8

--- components/Work.js
+++ components/Work.js
@@ -97,7 +97,7 @@ const Work = () => {
                         <div className="w-fit">
                             <Borders>
                                 <motion.p whileInView={paragraphAnimate} className="paragraph lg:w-[40ch] pt-3 pr-8">
-                                    Gallery for a skateboard shop. I created the assets and the UI/UX design. The layout went through several phases to save developer time.
+                                    UI/UX/Brand design for skateboard shop.
                                     <br /></motion.p></Borders>
                         </div><br />
                     </div>
@@ -113,6 +113,10 @@ const Work = () => {
                     /></Casestudyimage>
                 <Casestudyimage>
                     <img src='/moon3.png'
+                        alt="Tab version gallery shop"
+                    /></Casestudyimage>
+                <Casestudyimage>
+                    <img src='/moon4.png'
                         alt="Tab version gallery shop"
                     /></Casestudyimage>
 
