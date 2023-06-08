869cbf85bd6edd34bae78a9f33128374763fbf77
danielhdev <d.huebschmann@tutanota.com>
2023-06-08 16:09:57 +0200

    meta tag keyword update

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,8 @@ function Application({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="keywords" content="UI Web designer
 UI designer
-
+art direction
+art director
 Portfolio
 Design projects
 User interface
