58aba5b9d4605d7857c92807b62952899058c8b2
danielhdev <d.huebschmann@tutanota.com>
2023-06-09 17:54:19 +0200

    w update img component

--- components/Casestudyimage.js
+++ components/Casestudyimage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Casestudyimage = ({ children }) => {
     const fullWidth = React.Children.map(children, child =>
-        React.cloneElement(child, { className: 'max-w-[90vh]' }))
+        React.cloneElement(child, { className: 'w-full' }))
 
     return (
         <div className="w-full h-auto mx-auto my-[10%] lg:my-[5%]
