7c3745da71b69519addd7da3ed6dab29eef2dfbb
danielhdev <d.huebschmann@tutanota.com>
2023-08-03 18:06:55 +0200

    thursday update

--- pages/_app.js
+++ pages/_app.js
@@ -53,7 +53,7 @@ Frontend Development
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <title>UX/UI Design, Web Development - digital.dan</title>
+        <title>Web Development + UX/UI Design - digital.dan</title>
       </Head>
 
 
