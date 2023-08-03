e581c0e7affc283394849f5093eae31b5b34b115
danielhdev <d.huebschmann@tutanota.com>
2023-08-03 18:29:22 +0200

    twitter X update

--- components/Contacticons.js
+++ components/Contacticons.js
@@ -5,34 +5,35 @@ import Link from 'next/link'
 const Contacticons = () => {
     return (
         <div className='iconsize fixed bottom-3
-        flex justify-end space-x-[8vw] lg:space-x-[4vw] z-40'>
+        flex justify-end ] z-40'>
 
+            <div className="flex justify-between items-center space-x-[8vw] lg:space-x-[4vw]">
+                <Link href="mailto:d.huebschmann@tutanota.com"  >
+                    <a
+                        method="get"
+                        encType="text/plain">
+                        <ImMail2
+                            className='cursor-pointer'
+                        /></a>
+                </Link>
+                <Link
+                    href="https://twitter.com/digitaldan_de">
+                    <a target="_blank"
+                        rel="noopener noreferrer" >
+                        <span className='cursor-pointer'>X</span>
 
-            <Link href="mailto:d.huebschmann@tutanota.com"  >
-                <a
-                    method="get"
-                    encType="text/plain">
-                    <ImMail2
-                        className='cursor-pointer'
-                    /></a>
-            </Link>
-            <Link
-                href="https://twitter.com/digitaldan_de">
-                <a target="_blank"
-                    rel="noopener noreferrer" >
-                    <ImTwitter
-                        className='cursor-pointer'
-                    /></a></Link>
-            <Link
-                href="https://linkedin.com/in/daniel-huebschmann-175b66236/"
-            >
-                <a target="_blank"
-                    rel="noopener noreferrer" >
+                    </a></Link>
+                <Link
+                    href="https://linkedin.com/in/daniel-huebschmann-175b66236/"
+                >
+                    <a target="_blank"
+                        rel="noopener noreferrer" >
 
-                    <ImLinkedin
-                        className='cursor-pointer'
-                    /></a>
-            </Link>
+                        <ImLinkedin
+                            className='cursor-pointer'
+                        /></a>
+                </Link>
+            </div>
         </div>
     )
 }
