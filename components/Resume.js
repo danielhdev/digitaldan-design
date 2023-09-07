0dbbaf29ae3c7179e54c0608ba30db68397c1582
danielhdev <d.huebschmann@tutanota.com>
2023-09-07 07:21:01 +0200

    about section overhaul

--- components/Resume.js
+++ components/Resume.js
@@ -4,7 +4,7 @@ import ArrowLinks from "./utility/ArrowLinks"
 export default function Resume() {
     return (
         <>
-            <span className="py-[5%]">
+            <span className="pb-[5%]">
                 <Link href="https://drive.google.com/file/d/1JIKTwgwAZAFZ8fyAO0eYpzlEM95a5YWM/view?usp=drive_link"
                 >
                     <a target={'_blank'}
