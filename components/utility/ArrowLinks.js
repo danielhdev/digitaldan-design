fc3eacbcd55a69eb71c07bc4b7bfa2693616bb43
danielhdev <d.huebschmann@tutanota.com>
2023-09-13 10:52:37 +0200

    Link animation

--- components/utility/ArrowLinks.js
+++ components/utility/ArrowLinks.js
@@ -2,7 +2,8 @@ import { BsArrowUpRight } from 'react-icons/bs'
 
 const ArrowLinks = ({ children }) => {
     return (
-        <div className="subheader font-bold text-amber-600 cursor-pointer ml-[1.5vw] pl-[1.5vw] md:pl-0"
+        <div className="subheader font-bold text-amber-600 cursor-pointer ml-[1.5vw] pl-[1.5vw] md:pl-0
+        dark:hover:text-amber-100 hover:translate-x-[0.25rem] delay-50 duration-100"
         >{children} <BsArrowUpRight />
         </div>
     )
