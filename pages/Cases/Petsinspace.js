d3bff78e6b735a9d7b1ba7163690474d7ddc9be6
danielhdev <d.huebschmann@tutanota.com>
2023-10-10 12:41:44 +0200

    added pis

---
+++ pages/Cases/Petsinspace.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+import Borders from '@components/utility/Borders'
+import ArrowLinks from '@components/utility/ArrowLinks'
+import Back from '@components/utility/Back'
+import Projectspecs from '@components/utility/Projectspecs'
+import GalleryMapper from '@components/utility/GalleryMapper'
+
+const PetsInSpace = () => {
+    const tech = "Astro, Tailwind, Dall-e"
+    const role = "UI design, web development"
+    const desc = "Fun project with AI-generated images and tech playground. Roadmap: Pets in Space API, AI-supported image editor letting users create pictures of their pets as astronauts."
+
+    const paragraphAnimate = {
+        opacity: [0, 1],
+        y: [15, 0],
+        transition: { delay: 0.2, duration: 1 },
+    }
+    const images = [
+        '/ddimg1.jpg',
+        '/ddimg2.jpg',
+
+    ];
+    return (
+        <>
+            <main id="Work" className="h-auto flex 
+            flex-col justify-center items-center pb-[10rem] lg:pb-[15rem]">
+                <section className="lg:w-[80vw] h-auto flex flex-col justify-start gap-12 mx-auto mt-[15vh]">
+                    <div className="lg:w-[50%]">
+                        <h1 className="title">Pets In Space</h1>
+                        <div className="w-fit">
+                            <Borders>
+                                <motion.div whileInView={paragraphAnimate} className="paragraph lg:w-[40ch] pt-3 pr-8 ">
+                                    <Projectspecs
+                                        tech={tech}
+                                        role={role}
+                                        description={desc}
+                                    />
+                                </motion.div></Borders>
+                        </div><br />
+                        <Link href="/"
+                        >
+                            <a target={'_blank'}
+                                rel={'noreferrer noopener'}> <ArrowLinks >website
+                                </ArrowLinks>  </a>
+                        </Link>
+                    </div>
+                </section>
+                <GalleryMapper
+                    images={images} />
+                <Back />
+            </main>
+
+        </>
+
+    )
+}
+export default PetsInSpace
