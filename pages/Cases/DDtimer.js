385957d1288fa89d2bc6621cceaa21e0141d6686
danielhdev <d.huebschmann@tutanota.com>
2023-10-22 12:39:07 +0200

    + dd.timelogger -as portfolio

---
+++ pages/Cases/DDtimer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+import Borders from '@components/utility/Borders'
+import ArrowLinks from '@components/utility/ArrowLinks'
+import Back from '@components/utility/Back'
+import Projectspecs from '@components/utility/Projectspecs'
+import GalleryMapper from '@components/utility/GalleryMapper'
+
+const DDtimer = () => {
+    const tech = "NEXT, Zustand, Supabase"
+    const role = "UI design, web development"
+    const desc = "Demo - version of an ongoing project: d.d timelogger, a work hour log app, both simplistic and full of features. Including multi-platform use, safe data storage, statistics and an offline modus."
+
+    const paragraphAnimate = {
+        opacity: [0, 1],
+        y: [15, 0],
+        transition: { delay: 0.2, duration: 1 },
+    }
+    const images = [
+        '/ddtimermob.jpg',
+        '/ddtimerdesk.jpg',
+    ];
+    return (
+        <>
+            <main id="Work" className="h-auto flex 
+            flex-col justify-center items-center pb-[10rem] lg:pb-[15rem]">
+                <section className="lg:w-[80vw] h-auto flex flex-col justify-start gap-12 mx-auto mt-[15vh]">
+                    <div className="lg:w-[50%]">
+                        <h1 className="title">d.d timelogger</h1>
+                        <div className="w-fit">
+                            <Borders>
+                                <motion.div whileInView={paragraphAnimate} className="paragraph lg:w-[40ch] pt-3 pr-8 ">
+                                    <Projectspecs
+                                        tech={tech}
+                                        role={role}
+                                        description={desc}
+                                    />
+                                </motion.div></Borders>
+                        </div><br />
+                        <Link href="https://ddtimelogger.netlify.app/"
+                        >
+                            <a target={'_blank'}
+                                rel={'noreferrer noopener'}> <ArrowLinks >demo version (new tab)
+                                </ArrowLinks>  </a>
+                        </Link>
+                    </div>
+                </section>
+                <GalleryMapper
+                    images={images} />
+                <Back />
+            </main>
+
+        </>
+
+    )
+}
+export default DDtimer
