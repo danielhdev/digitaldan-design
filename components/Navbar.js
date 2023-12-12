f5d57487333519ab8235ab74398b0f1046579838
danielhdev <d.huebschmann@tutanota.com>
2023-12-12 19:27:09 +0100

    added desktop menut

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,6 +7,13 @@ const Navbar = () => {
     const [theme, setTheme] = useState('dark');
     const [isAnimating, setIsAnimating] = useState(false);
 
+    const menu = [
+        { id: 1, title: 'work', path: '/#Work' },
+        { id: 2, title: 'about', path: '/#About' },
+        { id: 3, title: 'contact', path: '/#Contact' },
+        { id: 4, title: 'resume', path: "https://drive.google.com/file/d/1JIKTwgwAZAFZ8fyAO0eYpzlEM95a5YWM/view?usp=drive_link" }
+    ]
+
 
     const toggleTheme = () => {
         setIsAnimating(true);
@@ -28,7 +35,7 @@ const Navbar = () => {
 
     return (
         <>
-            <div className="navbarcontainer z-10">
+            <main className="navbarcontainer z-10">
                 <nav className="flex items-center w-fit mx-auto space-x-[1.25rem] top-0 mt-[0.25rem] lg:ml-[1rem]">
                     <Link href="/">
                         <span className="font-[octarine] cursor-pointer mix-blend-difference logo">digital.dan</span>
@@ -47,11 +54,21 @@ const Navbar = () => {
                             </div>
                         </div>
                     </section>
-
+                    {menu.map((item) => {
+                        return (
+                            <nav className='hidden sm:block space-x-[1.25rem]'>
+                                <Link key={item.id} href={item.path}>
+                                    <a className="mix-blend-difference logo hover:text-amber-200">{item.title}
+                                    </a>
+                                </Link>
+                            </nav>
+                        )
+                    }
+                    )}
                     <Contacticons />
 
                 </nav>
-            </div >
+            </ main>
         </>
     )
 }
