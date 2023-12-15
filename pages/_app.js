2040eb5d96f635eb631e5923cc2ef57334a01903
danielhdev <d.huebschmann@tutanota.com>
2023-12-15 06:20:49 +0100

    UI updates

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ function Application({ Component, pageProps }) {
       <Head>
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="keywords" content="Web Developer Full Stack Developer
+        <meta name="keywords" content="Web Developer UI Designer
 
 Web Developer
 Web Development
@@ -35,23 +35,23 @@ APIs
 CMS
 "
         />
-        <meta name="description" content="Welcome to the portfolio of Daniel Hubschmann, Web Developer and Full Stack Developer." />
+        <meta name="description" content="Welcome to the portfolio of Daniel Hubschmann, Web Developer and UI Designer." />
 
         <meta http-equiv="X-UA-Compatible" content="IE=7" />
         <meta property="og:title" content="digital.dan Full Stack Web Development" />
-        <meta property="og:description" content="Welcome to the portfolio of Daniel Hubschmann, Web Developer, Full Stack Developer." />
+        <meta property="og:description" content="Welcome to the portfolio of Daniel Hubschmann, Web Developer, UI Designer." />
         <meta property="og:url" content="https://digitaldan.agency/" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Full Stack Developer Portfolio - Daniel Hubschmann digital.dan" />
-        <meta name="twitter:description" content="Welcome to the portfolio of Daniel Hubschmann digital.dan, Web Developer, Full Stack Developer." />
+        <meta name="twitter:title" content="Web Developer UI Designer Portfolio - Daniel Hubschmann digital.dan" />
+        <meta name="twitter:description" content="Welcome to the portfolio of Daniel Hubschmann digital.dan, Web Developer and UI Designer." />
         <meta name="twitter:image" content="https://example.com/your-image.jpg"></meta>
 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <title>Web Developer - digital.dan</title>
+        <title>UI Designer and Web Developer - digital.dan</title>
       </Head>
       <Navbar />
       <Menu />
